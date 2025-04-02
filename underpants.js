d5d0ad81e5c961912fabbc4394aca0593635c530
RRARI504 <ivorysimien56@gmail.com>
2025-04-02 15:00:50 -0500

    contains

--- underpants.js
+++ underpants.js
@@ -177,14 +177,9 @@ _.indexOf = function(array, value){
     for(var i = 0; i <= array.length; i++){
         if(array[i] === value){
             return i;
-
         }
-
-
-
     }
-    return -1
-
+    return -1;
     
 
 }
